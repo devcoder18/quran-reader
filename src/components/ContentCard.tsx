6f9b7892567856bde2805978b71993a317fbd164
devcoder18 <26886019+devcoder18@users.noreply.github.com>
2025-02-07 18:52:21 -0500

    feat: refactor ContentCard layout for improved structure and center alignment; update RTL styling

--- src/components/ContentCard.tsx
+++ src/components/ContentCard.tsx
@@ -11,38 +11,41 @@ interface ContentCardProps {
     loading: boolean;
 }
 
-const ContentCard: React.FC<ContentCardProps> = ({ 
+const ContentCard: React.FC<ContentCardProps> = ({
     title, verseText, lastWord, isLastVerseWord, verseNum, goToNextWord, goToPreviousWord, loading }) => {
 
     return (
-        <div className="flex grow">
-            <button
-                onClick={goToNextWord}
-                className="next-word-button">
-                <span className="material-icons">arrow_back</span>
-            </button>
-         
-            <div className="rtl rounded-lg border border-gray-300 p-4 shadow-md mx-4 grow">
-                <h2 className="text-2xl font-medium mb-2">{title}</h2>
-                {loading ? (
-                    <div className="flex justify-center items-center">
-                        <span className="material-icons">hourglass</span>
-                    </div>
-                ) : (
-                    <div className="rtl text-2xl md:text-2xl lg:text-3xl">
-                        {verseText} {lastWord && <span className='highlighted-word'>{lastWord}</span>}
-                        <span className={`verse-number ${isLastVerseWord ? 'verse-number-last' : 'verse-number-default'}`}>
-                            {verseNum.toLocaleString('ar-SA')}
-                        </span>
-                    </div>
-                )}
-            </div>
 
-            <button
-                onClick={goToPreviousWord}
-                className="next-word-button">
-                <span className="material-icons">arrow_forward</span>
-            </button>
+        <div className="flex-col w-full">
+            <h2 className="text-2xl font-medium mb-2 text-center">{title}</h2>
+            <div className="flex grow">
+                <button
+                    onClick={goToNextWord}
+                    className="next-word-button">
+                    <span className="material-icons">arrow_back</span>
+                </button>
+
+                <div className="text-center rtl rounded-lg border border-gray-300 p-4 shadow-md mx-4 grow">
+                    {loading ? (
+                        <div className="flex justify-center items-center">
+                            <span className="material-icons">hourglass</span>
+                        </div>
+                    ) : (
+                        <div className="rtl text-2xl md:text-2xl lg:text-3xl text-center">
+                            {verseText} {lastWord && <span className='highlighted-word'>{lastWord}</span>}
+                            <span className={`verse-number ${isLastVerseWord ? 'verse-number-last' : 'verse-number-default'}`}>
+                                {verseNum.toLocaleString('ar-SA')}
+                            </span>
+                        </div>
+                    )}
+                </div>
+
+                <button
+                    onClick={goToPreviousWord}
+                    className="next-word-button">
+                    <span className="material-icons">arrow_forward</span>
+                </button>
+            </div>
         </div>
     );
 };
