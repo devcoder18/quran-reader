818faf2c052850ef11fc7e9beaffbc76e7541b6e
devcoder18 <26886019+devcoder18@users.noreply.github.com>
2025-03-05 22:33:51 -0500

    feat: remove unused font files and update font paths to relative in globals.css

--- src/utils/baseUrl.ts
+++ src/utils/baseUrl.ts
@@ -1,10 +1,10 @@
 export function getBaseUrl(): { baseUrl: string } {
+  
+  if (typeof window !== 'undefined') {
+    const { pathname } = window.location;
+    const baseUrl = pathname.split('/').slice(0, -1).join('/');
+    return { baseUrl };
+  }
 
-    if (typeof window !== 'undefined') {
-        const { pathname } = window.location;
-        const baseUrl = pathname.split('/').slice(0, -1).join('/');
-        return { baseUrl };
-    } else {
-        return { baseUrl: '' };
-    }
+  return { baseUrl: '' };
 }
